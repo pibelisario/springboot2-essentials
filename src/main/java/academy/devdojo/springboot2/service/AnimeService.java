8709b3a2089e2eed6f2207df0bf30683d53201d1
Paulo Inácio <pibelisario@gmail.com>
2022-10-22 15:10:35 -0300

    Put

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -2,7 +2,6 @@ package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -34,5 +33,14 @@ public class AnimeService {
         anime.setId(ThreadLocalRandom.current().nextLong(3,10));
         animes.add(anime);
         return anime;
+    }
+
+    public void delete(Long id){
+       animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime){
+        delete(anime.getId());
+        animes.add(anime);
     }
 }

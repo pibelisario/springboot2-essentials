66d6660575ef86d4be4c2fd72c285eeb414f88c3
Paulo Inácio <pibelisario@gmail.com>
2022-10-22 12:01:23 -0300

    Post

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -6,12 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(0L, "DBZ"), new Anime(1L, "Naturo"), new Anime(2L,"Hunter"));
+    private static List<Anime> animes;
+
+    static{
+        animes = new ArrayList<>(List.of(new Anime(0L, "DBZ"), new Anime(1L, "Naturo"), new Anime(2L,"Hunter")));
+    }
 
     public List<Anime> list(){
         return animes;
@@ -22,5 +28,11 @@ public class AnimeService {
                 .filter(animes -> animes.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id not found"));
+    }
+
+    public Anime save(Anime anime){
+        anime.setId(ThreadLocalRandom.current().nextLong(3,10));
+        animes.add(anime);
+        return anime;
     }
 }
